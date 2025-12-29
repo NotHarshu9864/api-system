c73d1862a329a1ea8ba472cd4ba4b46fd16b5fa5
Harsh <notfamousharsh@gmail.com>
2025-12-29 19:45:53 +0530

    Update storage.js

--- authentication/core/storage.js
+++ authentication/core/storage.js
@@ -1,21 +1,21 @@
-async function getKey() {
-  let key = await crypto.subtle.generateKey(
-    { name: "AES-GCM", length: 256 },
-    true,
-    ["encrypt", "decrypt"]
-  );
-  return key;
+import { openDB } from "https://unpkg.com/idb?module";
+
+const DB_NAME = "FaceAuthDB";
+const STORE_NAME = "credentials";
+
+export async function saveEmbedding(embedding) {
+  const db = await openDB(DB_NAME, 1, {
+    upgrade(db) {
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME);
+      }
+    }
+  });
+
+  await db.put(STORE_NAME, embedding, "face");
 }
 
-export async function encryptData(data) {
-  const key = await getKey();
-  const iv = crypto.getRandomValues(new Uint8Array(12));
-
-  const encrypted = await crypto.subtle.encrypt(
-    { name: "AES-GCM", iv },
-    key,
-    new TextEncoder().encode(JSON.stringify(data))
-  );
-
-  return { encrypted, iv, key };
+export async function loadEmbedding() {
+  const db = await openDB(DB_NAME, 1);
+  return await db.get(STORE_NAME, "face");
 }
