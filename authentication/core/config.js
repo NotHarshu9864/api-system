69189e9113cb8f8c243078da3e86967607255422
Harsh <notfamousharsh@gmail.com>
2025-12-29 19:43:12 +0530

    Create authentication/core/config.js

---
+++ authentication/core/config.js
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  FACE_DISTANCE_THRESHOLD: 0.45,
+  LIVENESS_BLINKS_REQUIRED: 2,
+  ENCRYPTION_KEY_NAME: "face_auth_key",
+  DB_NAME: "FaceAuthDB",
+  STORE_NAME: "credentials"
+};
